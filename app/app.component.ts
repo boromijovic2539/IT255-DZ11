c91478d15e62e078f8215354d074f6d6e30924f6
boromijovic2539 <bmmaradoo@googlemail.com>
2016-06-10 01:48:54 +0200

    Initial commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,24 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig,Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import { HomeComponent } from 'app/home/home.component';
 import { FindRoomComponent } from 'app/findroom/findroom.component';
 import { LoginComponent } from 'app/login/login.component';
 import { RegisterComponent } from 'app/register/register.component';
+import { AddRoomComponent } from 'app/addroom/addroom.component';
 import {Pipe} from 'angular2/core';
+
 @Component({
  selector: 'moja-aplikacija',
  templateUrl: 'app/router.html',
  directives: [ROUTER_DIRECTIVES]
 })
+
 @RouteConfig([
  {path:'/FindRoom', name: 'FindRoom', component: FindRoomComponent},
  {path:'/Login', name:'Login', component: LoginComponent},
  {path:'/Home', name:'Home', component: HomeComponent, useAsDefault: true},
- {path:'/Register', name:'Register', component: RegisterComponent}
+ {path:'/Register', name:'Register', component: RegisterComponent},
+ {path:'/AddRoom', name:'AddRoom', component: AddRoomComponent}
  ])
-export class AppComponent {
+export class AppComponent { 
 }
--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, Directive } from 'angular2/core';
-import {ControlGroup} from 'angular2/common';
+import {Component,Directive} from 'angular2/core';
+
+
 @Component({
  selector: 'Login',
  templateUrl: 'app/login/login.html',
